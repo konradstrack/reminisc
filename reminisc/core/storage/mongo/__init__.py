abe97d6933a7215517093695f90ddcc74f268132
Konrad Strack <konrad@strack.pl>
2013-06-09 10:20:44 +0200

    small code formatting improvements

--- reminisc/core/storage/mongo/__init__.py
+++ reminisc/core/storage/mongo/__init__.py
@@ -24,32 +24,42 @@ class MongoDbStorage(Storage):
 		username = self.config.get('database', 'username', fallback=None)
 		password = self.config.get('database', 'password', fallback=None)
 
-		mongoengine.connect(database_name, host=host, port=port, username=username, password=password)
+		mongoengine.connect(database_name, host=host, port=port, username=username, 
+			password=password)
 
 
 	def store_message(self, command):
 		# get account and contact references
 		try:
-			hints = [self.__get_account(handle, command) for handle in command.account_hints] if command.account_hints is not None else []
-			print(command.account_hints)
+			if command.account_hints is not None:
+				hints = [self.__get_account(handle, command) for handle in command.account_hints]
+			else:
+				hints = []
+
 			account = self.__get_account(command.account_id, command)
 		except MultipleObjectsReturned:
-			logger.error('Multiple accounts found for [{}, {}, {}]. Skipping message.'.format(command.account_id, 
-				command.source, command.protocol))
+			logger.error('Multiple accounts found for [{}, {}, {}]. Skipping message.'.format(
+				command.account_id, command.source, command.protocol))
 			return
 
 		try:
 			contact = self.__get_contact(command)
 		except MultipleObjectsReturned:
-			logger.error('Multiple contacts found for [{}, {}, {}]. Skipping message.'.format(command.contact_id, 
-				command.source, command.protocol))
+			logger.error('Multiple contacts found for [{}, {}, {}]. Skipping message.'.format(
+				command.contact_id, command.source, command.protocol))
 			return
 
 		datetime = command.datetime
 		message = command.message
-		direction = 'Received' if command.direction == commands.NewMessage.Direction.RECEIVED else 'Sent'
 
-		Message(account=account, contact=contact, datetime=datetime, direction=direction, message=message).save()
+		if command.direction == commands.NewMessage.Direction.RECEIVED:
+			direction = 'Received' 
+		else:
+			direction = 'Sent'
+
+		Message(account=account, contact=contact, datetime=datetime, 
+			direction=direction, message=message).save()
+
 		logger.debug("Message ({}): {}".format(command.direction, command.message))
 
 	def __get_account(self, handle, command):
