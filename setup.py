b97e79d1cd7cdbd54eef5d580f62540d6ec24ea6
Konrad Strack <konrad@strack.pl>
2014-03-03 20:17:02 +0100

    a proper setup.py with py.test integration

--- setup.py
+++ setup.py
@@ -1,16 +1,46 @@
-import os
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
-def read(fname):
+import os
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+def read_file(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "reminisc",
-    version = "0.0.1-SNAPSHOT",
-    author = "Konrad Strack",
-    author_email = "konrad@strack.pl",
-    description = (""),
-    license = "BSD",
-    packages=['reminisc', 'tests'],
-    long_description=read('README'),
+    name="reminisc",
+    version="0.1-SNAPSHOT",
+    author="Konrad Strack",
+    author_email="",
+    description=(""),
+    long_description=read_file('README'),
+    license="BSD",
+    url="https://github.com/konradstrack/reminisc",
+    packages=find_packages(),
+
+    install_requires=[
+        'mongoengine==0.8.7'
+    ],
+
+    package_data={
+    },
+
+    tests_require=['pytest', 'pytest-cov'],
+    cmdclass={'test': PyTest},
 )
