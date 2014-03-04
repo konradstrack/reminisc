8ff0aadb12fd0a4dfee90abc3aa7ffc497e83f0d
Konrad Strack <konrad@strack.pl>
2014-03-04 22:34:44 +0100

    add coverage to setup.py test command

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sys
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+        self.test_args = ["--cov", "reminisc"]
         self.test_suite = True
 
     def run_tests(self):
